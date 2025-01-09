2a9b059466f62a37c7c991794521208f14f721ff
Yaoyao Hu <shady030314@gmail.com>
2025-01-09 10:29:25 +0800

    add gdb-dashboard for c

--- c/build.sh
+++ c/build.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
 docker rmi yaoyhu/gcc:12.4 2>/dev/null
-docker build -t yaoyhu/gcc:12.4 .
+docker build --network host --build-arg http_proxy=http://127.0.0.1:7890 \
+  --build-arg https_proxy=http://127.0.0.1:7890 \
+  -t yaoyhu/gcc:12.4 .
